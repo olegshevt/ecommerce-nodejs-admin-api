79b54b5e048e1e6e4a79938ddef81bdba90921a0
olegshevt <olegshevt@gmail.com>
2020-11-08 19:34:02 +0300

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,74 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+
+exports.getLogin = (req, res, next) => {
+
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+    });
+
+};
+
+exports.postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email: email }).then(user => {
+        if (!user) {
+            return res.redirect('/login');
+        }
+        bcrypt.compare(password, user.password).then(match => {
+            if (match) {
+                req.session.isLoggedIn = true;
+                req.session.user = user;
+                return req.session.save(err => {
+                    console.log(err);
+                    res.redirect('/');
+                });
+            }
+            res.redirect('/login');
+        })
+    }).catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    })
+};
+
+exports.getSignup = (req, res, next) => {
+
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
+
+};
+
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
+        .then(userDoc => {
+            if (userDoc) {
+                return res.redirect('/signup');
+            }
+            return bcrypt.hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: { items: [] }
+                    });
+                    return user.save();
+                }).then(result => {
+                    res.redirect('/login');
+                })
+        })
+        .catch(err => { console.log(err) })
+}
