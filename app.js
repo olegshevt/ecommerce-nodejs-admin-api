db4803b4a55b11fe1a7f42e7157de4ab0fef7458
olegshevt <olegshevt@gmail.com>
2020-11-15 20:31:37 +0300

    Ready for deployment

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
-
-
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,10 +8,14 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
-const MONGO_URI = 'mongodb+srv://oleg:Puzey3JdnkV3yAqb@cluster0.dbffk.mongodb.net/shop';
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.dbffk.mongodb.net/${process.env.MONGO_DATABASE}`;
 const multer = require('multer');
 const flash = require('connect-flash');
 const app = express();
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const https = require('https');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -49,6 +52,11 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' }
+);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('imageUrl')
 );
@@ -62,6 +70,9 @@ app.use(session({
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(csrfProtection);
 app.use(flash());
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use((req, res, next) => {
 
@@ -89,11 +100,16 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
+
 mongoose
   .connect(
     MONGO_URI, { useNewUrlParser: true })
   .then(result => {
-    app.listen(3000);
+    // https.createServer({ key: privateKey, cert: certificate }, app)
+    //   .listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
